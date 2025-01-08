b2940750aa0bbb98be3524d7e087ba79b3e8d627
阿木 <sennes111762@gmail.com>
2025-01-08 13:56:00 +0800

    ✨ feat: Add matter-js library and floating text animation
    Added matter-js library for physics simulation and created a floating text animation component displaying "山衔" and "雁引".

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="app-container">
-    <div class="decorative-text left">山衔</div>
-    <div class="decorative-text right">雁引</div>
+    <FloatingText />
     <div class="content-wrapper">
       <h1 class="title">和侄子今天做什么？</h1>
       <ImageSelector />
@@ -11,11 +10,13 @@
 
 <script>
 import ImageSelector from './components/ImageSelector.vue'
+import FloatingText from './components/FloatingText.vue'
 
 export default {
   name: 'App',
   components: {
-    ImageSelector
+    ImageSelector,
+    FloatingText
   }
 }
 </script>
@@ -55,81 +56,14 @@ html, body {
   width: 100%;
   padding: 20px;
   position: relative;
-}
-
-.decorative-text {
-  position: fixed;
-  font-size: 72px;
-  color: rgba(255, 217, 102, 0.9);
-  -webkit-writing-mode: vertical-rl;
-  writing-mode: vertical-rl;
   z-index: 1;
-  padding: 0.5em 0.3em;
-  text-shadow: 
-    2px 2px 0 rgba(255, 166, 0, 0.4),
-    -2px -2px 0 rgba(255, 255, 255, 0.4);
-  line-height: 1.2;
-  max-height: 4em;
-  overflow: hidden;
-  text-align: center;
-  top: 50%;
-  -webkit-transform: translateY(-50%);
-  transform: translateY(-50%);
-  font-weight: 600;
-  letter-spacing: 0.1em;
-}
-
-.left {
-  left: -0.2em;
-}
-
-.right {
-  right: -0.2em;
-}
-
-@media screen and (max-width: 768px) {
-  .decorative-text {
-    position: absolute;
-    font-size: 48px;
-    color: rgba(255, 230, 140, 1);
-    text-shadow: 
-      2px 2px 4px rgba(0, 0, 0, 0.3),
-      -1px -1px 0 rgba(255, 255, 255, 0.5);
-    padding: 0;
-  }
-  
-  .left {
-    left: 0;
-    margin-left: 10px;
-    text-align: start;
-  }
-  
-  .right {
-    right: 0;
-    margin-right: 10px;
-    text-align: end;
-  }
-}
-
-@media screen and (max-width: 480px) {
-  .decorative-text {
-    font-size: 36px;
-  }
-  
-  .left {
-    margin-left: 5px;
-  }
-  
-  .right {
-    margin-right: 5px;
-  }
 }
 
 .title {
   font-size: 2.5rem;
   color: #fff;
   text-align: center;
-  margin-bottom: 2rem;
+  margin-bottom: 30px;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
   background: rgba(0, 0, 0, 0.4);
   padding: 1rem;
